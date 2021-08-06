04e4d9ad0e0af80c358c805b327bb04bf9e44a2e
YuChuQing <41681040+YuChuQing@users.noreply.github.com>
2021-08-06 14:24:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,6 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 #默认开放80端口
-sed -i '/# special user chains, e.g. input_wan_rule or postrouting_lan_rule./a iptables -I INPUT -p tcp  -m multiport --dport 80 -j ACCEPT' package/network/config/firewall/files/firewall.user
+sed -i '/input_wan_rule or postrouting_lan_rule/a iptables -I INPUT -p tcp  -m multiport --dport 80 -j ACCEPT' package/network/config/firewall/files/firewall.user
 #修改最大连接数为65535
-sed -i '/# Defaults are configured in /etc/sysctl.d/* and can be customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
